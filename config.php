85707793c552bb4fbab95094d2570c0abf67c6a6
Fahrul Yurisnan <fahrul4215@gmail.com>
2018-11-20 20:30:30 +0700

    config + database

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/SI_Pelayanan_Antrian_Puskesmas_Server/';
+$config['base_url'] = 'http://serverpuskesmas.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/SI_Pelayanan_Antrian_Puskesmas_Server/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
